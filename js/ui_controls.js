26c5560f1d6eea1e0310e2fd8376aefefa8dd69d
nadavids42 <ndavidson42@gmail.com>
2025-04-24 20:21:51 -0400

    Update ui_controls.js

--- js/ui_controls.js
+++ js/ui_controls.js
@@ -1,8 +1,5 @@
-// ui_controls.js
 export function setupControls(
   metricSelect,
-  xMetricSelect,
-  yMetricSelect,
   yearSlider,
   rerender,
   minYear,
@@ -10,20 +7,16 @@ export function setupControls(
   defaultYear,
   METRICS
 ) {
-  // Populate all three dropdowns with metric options
-  [metricSelect, xMetricSelect, yMetricSelect].forEach(select => {
-    select.selectAll("option").remove();
-    select.selectAll("option")
-      .data(METRICS)
-      .enter().append("option")
-      .attr("value", d => d.key)
-      .text(d => d.label);
-  });
+  // Populate metric dropdown
+  metricSelect.selectAll("option").remove();
+  metricSelect.selectAll("option")
+    .data(METRICS)
+    .enter().append("option")
+    .attr("value", d => d.key)
+    .text(d => d.label);
 
-  // Set initial values (customize as needed)
+  // Set initial value
   metricSelect.property("value", "grad");
-  xMetricSelect.property("value", "salary");
-  yMetricSelect.property("value", "grad");
 
   yearSlider
     .attr("min", minYear)
@@ -33,6 +26,4 @@ export function setupControls(
     .on("input", rerender);
 
   metricSelect.on("change", rerender);
-  xMetricSelect.on("change", rerender);
-  yMetricSelect.on("change", rerender);
 }
