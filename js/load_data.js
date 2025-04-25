aa9a4393f3252f8aa4d5749232c5969d199dcdba
nadavids42 <ndavidson42@gmail.com>
2025-04-24 20:22:26 -0400

    Update load_data.js

--- js/load_data.js
+++ js/load_data.js
@@ -1,8 +1,7 @@
-// load_data.js
 export function loadData() {
   return Promise.all([
     d3.json("/data/SchoolDistricts_poly.geojson"),
     d3.csv("/data/Cleaned_data.csv"),
-    d3.json("/data/Massachusetts_districts.geojson") // Add your new layer here
+    d3.json("/data/Massachusetts_districts.geojson")
   ]);
 }
